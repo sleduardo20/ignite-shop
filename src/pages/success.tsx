272367155b5123be9867ab1a3b906ff99776713b
sleduardo20 <sl.eduardo20@gmail.com>
2023-04-24 17:11:28 -0300

    🎨 add styles success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,11 +1,13 @@
 import { ImageContainer, SuccessContainer } from '@/styles/pages/success';
+import Link from 'next/link';
 
 export default function Success() {
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
       <ImageContainer></ImageContainer>
-      <p>Uhhuu, você concluiu sua compra com sucesso !!!</p>
+      <p>Uhhuu, Eduardo sua camiseta ja esta a caminho de seu endereço !!!</p>
+      <Link href="/">Voltar ao catálago</Link>
     </SuccessContainer>
   );
 }
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -1,4 +1,53 @@
-import { styled } from '@stitches/react';
+import { styled } from '..';
 
-export const SuccessContainer = styled('main', {});
-export const ImageContainer = styled('div', {});
+export const SuccessContainer = styled('main', {
+  flexDirection: 'column',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  margin: '0 auto',
+  height: 656,
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray100',
+  },
+
+  p: {
+    marginTop: '2rem',
+    fontSize: '$xl',
+    color: '$gray300',
+    maxWidth: 560,
+    textAlign: 'center',
+    lineHeight: '1.4rem',
+  },
+
+  a: {
+    display: 'block',
+    marginTop: '5rem',
+    fontSize: 'large',
+    fontWeight: 'bold',
+    color: '$green500',
+    textDecoration: 'none',
+
+    '&:hover': {
+      color: '$green300',
+    },
+  },
+});
+export const ImageContainer = styled('div', {
+  marginTop: '4rem',
+  width: '100%',
+  background: 'linear-gradient(180deg,#1ea483 0%, #7465d4 100% )',
+  maxWidth: 130,
+  height: 145,
+  borderRadius: 8,
+  padding: '0.25rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+});
